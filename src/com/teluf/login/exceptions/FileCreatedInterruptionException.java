ff1b025c22a6758c9497be2464f102f0dae2bd8c
teluf <64613767+teluf@users.noreply.github.com>
2021-07-28 23:28:52 -0700

    Create FileCreatedInterruptionException.java

---
+++ src/com/teluf/login/exceptions/FileCreatedInterruptionException.java
@@ -0,0 +1,8 @@
+package com.teluf.login.exceptions;
+
+@SuppressWarnings("serial")
+public class FileCreatedInterruptionException extends Exception {
+	public FileCreatedInterruptionException(String msg) {
+		super(msg);
+	}
+}
